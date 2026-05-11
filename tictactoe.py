54617f572d26c62472ef73efe3cdbbcae857f5f7
Vincent Chen <vincent.chen18@education.nsw.gov.au>
2026-05-11 18:30:31 +1000

    bugfix for TTT

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,9 @@
 def check_winner(board):
-    lines = [(0,1,2),(3,4,5),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
+    lines = [(0,1,2),(3,4,5),(6,7,8),    # rows
+             (0,3,6),(1,4,7),(2,5,8),    # columns
+             (0,4,8),(2,4,6)]             # diagonals
     for a,b,c in lines:
-        if board[a] == board[b] == board[c]:
+        if board[a] == board[b] == board[c] and board[a] != ' ':
             return board[a]
     return None
 ## minimax algo is for the bot. If human vs human, separate function for winchecking.
