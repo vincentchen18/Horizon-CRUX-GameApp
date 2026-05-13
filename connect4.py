b51511d2d3d8c84b4267fdb3101a47b90e5ec9aa
Vincent Chen <vincent.chen18@education.nsw.gov.au>
2026-05-14 08:21:18 +1000

    connect4 FINISHED :)))))

--- connect4.py
+++ connect4.py
@@ -5,14 +5,14 @@ winning_lines = [(1,2,3,4),(2,3,4,5),(3,4,5,6),(4,5,6,7),(8,9,10,11),(9,10,11,12
                      (4,10,16,22),(5,11,17,23),(6,12,18,24),(7,13,19,25),(11,17,23,29),(12,18,24,30),(13,19,25,31),(14,20,26,32),(18,24,30,36),(19,25,31,37),(20,26,32,38),(21,27,33,39)] #backward diagonals
 def connect4_check_win(board):
     for a,b,c,d in winning_lines:
-        if board[a-1] == board[b-1] == board[c-1] == board[d-1] and board[a-1] != ' ':
+        if board[a-1] == board[b-1] == board[c-1] == board[d-1] and board[a-1] != '⬛':
             return board[a-1]
     return None
 
 def connect4_get_drop_cell(board, col):
     # give a column, return which cell the counter is dropped to
     column_cells = [col + 7*r for r in range(6)]
-    empties = [x for x in column_cells if board[x] == ' ']
+    empties = [x for x in column_cells if board[x] == '⬛']
     if not empties:
         return None
     return max(empties)
@@ -24,7 +24,7 @@ def connect4_evaluate(board, bot_colour, human_colour):
         window = [board[a-1],board[b-1],board[c-1],board[d-1]]
         bot_count = window.count(bot_colour)
         human_count = window.count(human_colour)
-        empty_count = window.count(' ')
+        empty_count = window.count('⬛')
 
         if bot_count == 4: # winning position
             score += 1000
@@ -54,7 +54,7 @@ def connect4_minimax(board, depth, is_bot_turn, alpha, beta, bot_colour, human_c
         return 100000
     if winner == human_colour:
         return -100000
-    if ' ' not in board:
+    if '⬛' not in board:
         return 0
     if depth == 0:
         return connect4_evaluate(board, bot_colour, human_colour)
@@ -68,7 +68,7 @@ def connect4_minimax(board, depth, is_bot_turn, alpha, beta, bot_colour, human_c
                 continue
             board[cell] = bot_colour
             score = connect4_minimax(board, depth-1, False, alpha, beta, bot_colour, human_colour)
-            board[cell] = ' '
+            board[cell] = '⬛'
             best = max(best, score)
             alpha = max(alpha, best)
             if beta <= alpha: #prune the tree
@@ -81,7 +81,7 @@ def connect4_minimax(board, depth, is_bot_turn, alpha, beta, bot_colour, human_c
                 continue
             board[cell] = human_colour
             score = connect4_minimax(board, depth-1, True, alpha, beta, bot_colour, human_colour)
-            board[cell] = ' '
+            board[cell] = '⬛'
             best = min(best, score)
             if beta <= alpha:
                 break
@@ -96,14 +96,14 @@ def connect4_botmove(board, bot_colour, human_colour, difficulty): # determine b
             continue
         board[cell] = bot_colour
         score = connect4_minimax(board, difficulty-1, False, -999999, 999999, bot_colour, human_colour)
-        board[cell] = ' '
+        board[cell] = '⬛'
         if score > best_score:
             best_score, best_column = score, column
     return best_column
 
 def connect4_print_board(board):
     print()
-    print(' ' + "   ".join(list(map(str, range(1,8)))))
+    print('  ' + " ⬛ ".join(list(map(str, range(1,8)))))
     for r in range(6):
         print_cells = [board[r*7+c] for c in range(7)]
         print("| " + " | ".join(print_cells) + " |")
@@ -117,14 +117,15 @@ def connect4_humanmove(board, label, color):
             if a > 7 or a < 1:
                 print('Please select a column 1-7.')
                 continue
-            if connect4_get_drop_cell(board, a) is None:
+            if connect4_get_drop_cell(board, a-1) is None:
                 print("That column is full!")
                 continue
             else:
-                return a
+                return a-1
         except ValueError:
             print('Please select a column 1-7.')
             continue
+
 
 
 def connect4_play():
@@ -141,7 +142,7 @@ def connect4_play():
             return
         else:
             print("Please enter 1,2 or 3.")
-    board = [' '] * 42
+    board = ['⬛'] * 42
     current = '🔴'
     #2 - if bot, ask if go first or 2nd
     if vs_bot:
@@ -150,10 +151,12 @@ def connect4_play():
             if raw == '1':
                 human_colour = '🔴'
                 bot_colour = '🔵'
+                is_bot_turn = False
                 break
             elif raw == '2':
                 human_colour = '🔵'
                 bot_colour = '🔴'
+                is_bot_turn = True
                 break
             else:
                 print("Please enter 1 or 2.")
@@ -161,4 +164,40 @@ def connect4_play():
         bot_colour = None
         human_colour = None
     # main game loop
+    while True:
+        connect4_print_board(board)
+        w = connect4_check_win(board)
+        if vs_bot:
+            if w == bot_colour:
+                print("Vinniebot wins! 🤖")
+                break
+            elif w == human_colour:
+                print("You win! 🎉")
+                break
+        else:
+            if w == '🔴':
+                print('Player 🔴 wins!')
+                break
+            elif w == '🔵':
+                print('Player 🔵 wins!')
+                break
+        if '⬛' not in board:
+            print("It's a tie!")
+            break
+        if vs_bot:
+            if is_bot_turn:
+                movecell = connect4_get_drop_cell(board, connect4_botmove(board, bot_colour, human_colour, 5))
+                board[movecell] = bot_colour
+            else:
+                movecell = connect4_get_drop_cell(board, connect4_humanmove(board, 'You', human_colour))
+                board[movecell] = human_colour
+        else:
+            label = f'Player {current}'
+            movecell = connect4_get_drop_cell(board, connect4_humanmove(board, label, human_colour))
+            board[movecell] = current
+        current = '🔴' if current == '🔵' else '🔵'
+        if vs_bot:
+            is_bot_turn = not is_bot_turn
 
+
+connect4_play()
