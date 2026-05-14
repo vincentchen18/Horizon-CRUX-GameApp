a1997741794ca96497289d423cb16bbc3bdf6633
Vincent Chen <vincent.chen18@education.nsw.gov.au>
2026-05-14 12:38:54 +1000

    add wagering

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,8 @@
 suits = "♥♦♣♠"
 nums = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 deck = [nums[i] + suits[j] for i in range(13) for j in range(4)]
-import random
+
+import random, time
 
 def blackjack_setup():
     random.shuffle(deck)
@@ -10,6 +11,7 @@ def blackjack_setup():
 
 
 def blackjack_play():
+    balance = 1000
     while True:
         a = input("1) Play Blackjack    2) Exit    Please enter 1 or 2: ").strip()
         if a == "1":
@@ -18,7 +20,31 @@ def blackjack_play():
             return
         else:
             print("Invalid input. Please enter 1 or 2.")
+
+    while True:
+        print(f"Your balance is ${balance}, how much would you like to wager? Please enter an integer.")
+        money = input("$")
+        try:
+            money = int(money)
+            if money <= 0:
+                print("Wager must be greater than zero.")
+                continue
+            elif money > balance:
+                print("You don't have enough money to wager this!.")
+                continue
+            else:
+                print(f"Wagering ${money}.")
+                break
+        except ValueError:
+            print("Please enter a number.")
+            continue
+    # wager
+
     mycard1, mycard2, dealercard1, dealercard2 = blackjack_setup()
     print("Shuffling the cards", end="")
     for i in range(3):
-        pass
+        time.sleep(0.4)
+        print(".",end="")
+    time.sleep(0.4)
+    print()
+    print(f"Your cards are {mycard1} and {mycard2}")
