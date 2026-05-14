33a9da43053544eca3e092218905772f9ed81f1e
Vincent Chen <vincent.chen18@education.nsw.gov.au>
2026-05-14 10:02:41 +1000

    small display change but connect4 done! :))

--- connect4.py
+++ connect4.py
@@ -1,4 +1,5 @@
 # 7x6 board
+import time
 winning_lines = [(1,2,3,4),(2,3,4,5),(3,4,5,6),(4,5,6,7),(8,9,10,11),(9,10,11,12),(10,11,12,13),(11,12,13,14),(15,16,17,18),(16,17,18,19),(17,18,19,20),(18,19,20,21),(22,23,24,25),(23,24,25,26),(24,25,26,27),(25,26,27,28),(29,30,31,32),(30,31,32,33),(31,32,33,34),(32,33,34,35),(36,37,38,39),(37,38,39,40),(38,39,40,41),(39,40,41,42), # horizontal winning lines
                      (1,8,15,22),(2,9,16,23),(3,10,17,24),(4,11,18,25),(5,12,19,26),(6,13,20,27),(7,14,21,28),(8,15,22,29),(9,16,23,30),(10,17,24,31),(11,18,25,32),(12,19,26,33),(13,20,27,34),(14,21,28,35),(15,22,29,36),(16,23,30,37),(17,24,31,38),(18,25,32,39),(19,26,33,40),(20,27,34,41),(21,28,35,42), #vertical wins
                      (1,9,17,25),(2,10,18,26),(3,11,19,27),(4,12,20,28),(8,16,24,32),(9,17,25,33),(10,18,26,34),(11,19,27,35),(15,23,31,39),(16,24,32,40),(17,25,33,41),(18,26,34,42), #forward diagonals
@@ -189,10 +190,16 @@ def connect4_play():
             break
         if vs_bot:
             if is_bot_turn:
+                print("Vinniebot is thinking", end="")
+                for i in range(3):
+                    time.sleep(0.4)
+                    print(".", end="")
+                time.sleep(0.4)
                 move = connect4_botmove(board, bot_colour, human_colour, 5)
                 movecell = connect4_get_drop_cell(board, move)
                 message = f"Vinniebot plays column {move+1}."
                 board[movecell] = bot_colour
+                print()
             else:
                 movecell = connect4_get_drop_cell(board, connect4_humanmove(board, 'You', human_colour))
                 board[movecell] = human_colour
